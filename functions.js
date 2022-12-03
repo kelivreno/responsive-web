6ca0e08e10f5033594eab5699eda7eb2d0b863aa
kelivreno <hakim.kelangit@student.greenriver.edu>
2022-12-02 23:31:09 -0800

    nothing much yet

---
+++ functions.js
@@ -0,0 +1,8 @@
+// space, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("space")
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("tomato")
+// light, dark
+favouriteMode("light")
+// sharp, soft, round
+favouriteEdgeStyle("sharp")
